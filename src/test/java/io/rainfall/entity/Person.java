30172ef890c273041464571eadd5c5b31c4d695c
Aurelien Broszniowski <Aurelien.Broszniowski@softwareag.com>
2019-01-03 17:18:47 +0100

    remove jsr166 module and keep only LongAdder / DoubleAdder

--- src/test/java/io/rainfall/entity/Person.java
+++ src/test/java/io/rainfall/entity/Person.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Aurélien Broszniowski
+ * Copyright (c) 2014-2019 Aurélien Broszniowski
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,10 @@
 
 package io.rainfall.entity;
 
-import jsr166e.ThreadLocalRandom;
 
 import com.datastax.driver.mapping.annotations.Table;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author Aurelien Broszniowski
--- src/test/java/io/rainfall/entity/PersonGenerator.java
+++ src/test/java/io/rainfall/entity/PersonGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Aurélien Broszniowski
+ * Copyright (c) 2014-2019 Aurélien Broszniowski
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,8 @@
 package io.rainfall.entity;
 
 import io.rainfall.ObjectGenerator;
-import jsr166e.ThreadLocalRandom;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author Aurelien Broszniowski
